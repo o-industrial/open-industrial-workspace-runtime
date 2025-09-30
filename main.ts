1ab11fc1f2a30b600efbdbd36808613b5b064aa3
mcgear <michael.gearhardt@gmail.com>
2025-09-30 12:20:00 -0600

    log

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import { start } from '@fathym/eac/runtime/server';
 import { config, configure } from './configs/eac-runtime.config.ts';
 
+console.log("===========================================>Let's Go")
+
 await start(await config, { configure });
