2495807bca53963f5a314ccb86b848b3634c25a7
mcgear <michael.gearhardt@gmail.com>
2025-10-17 13:14:27 -0600

    ref updates

--- src/plugins/RuntimePlugin.ts
+++ src/plugins/RuntimePlugin.ts
@@ -31,7 +31,7 @@ import {
   resolveAccessRights,
 } from '@o-industrial/common/runtimes';
 import { EaCMSALProcessor } from '@fathym/msal';
-import { fromFileUrl } from '@std/path/from-file-url';
+// import { fromFileUrl } from '@std/path/from-file-url';
 
 export default class RuntimePlugin implements EaCRuntimePlugin {
   constructor() {}
@@ -279,21 +279,21 @@ export default class RuntimePlugin implements EaCRuntimePlugin {
             // } as EaCLocalDistributedFileSystemDetails,
           },
           'jsr:@o-industrial/oi-core-pack': {
-            // Details: {
-            //   Type: 'JSR',
-            //   Package: '@o-industrial/oi-core-pack',
-            //   Version: '',
-            //   Extensions: ['tsx', 'ts'],
-            // } as EaCJSRDistributedFileSystemDetails,
             Details: {
-              Type: 'Local',
-              FileRoot: fromFileUrl(
-                import.meta.resolve(
-                  '../../../../o-industrial/oi-core-pack/',
-                ),
-              ),
+              Type: 'JSR',
+              Package: '@o-industrial/oi-core-pack',
+              Version: '',
               Extensions: ['tsx', 'ts'],
-            } as EaCLocalDistributedFileSystemDetails,
+            } as EaCJSRDistributedFileSystemDetails,
+            // Details: {
+            //   Type: 'Local',
+            //   FileRoot: fromFileUrl(
+            //     import.meta.resolve(
+            //       '../../../../o-industrial/oi-core-pack/',
+            //     ),
+            //   ),
+            //   Extensions: ['tsx', 'ts'],
+            // } as EaCLocalDistributedFileSystemDetails,
           },
         },
         Modifiers: {
